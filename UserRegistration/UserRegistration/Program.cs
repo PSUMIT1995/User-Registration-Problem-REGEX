a86f51645830a200106be5083d796efceb7ff703
PSUMIT1995 <psumit191.sp@gmail.com>
2022-04-10 02:33:18 +0530

    UC9 : Testing Email Samples

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,16 +8,16 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Regex User Registration Problem");
-            Console.WriteLine("Enter Password");
+            Console.WriteLine("Enter EmailId");
 
             string inStr = Console.ReadLine();
             string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]"; 
+            string emailId = "^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]+@[a-zA-Z0-9]{1,10}[.][a-zA-Z]{2,10}[.]*[a-zA-Z]*$";
             string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
-            string password = @"[a-zA-Z0-9]{1}[A-Z][0-9][@#$%&*_-]+[a-zA-Z0-9]$";//creating pattern for password
+            string password = @"[a-zA-Z0-9]{1}[A-Z][0-9][@#$%&*_-]+[a-zA-Z0-9]$"; 
 
-            Regex regex = new Regex(password);
+            Regex regex = new Regex(emailId);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
