93863e65bdaf5fce7fa12500d98e7c5eea6f2847
PSUMIT1995 <psumit191.sp@gmail.com>
2022-04-10 02:06:01 +0530

    UC5 : Enter a password minimum 8 characters

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,15 +8,16 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Regex User Registration Problem");
-            Console.WriteLine("Enter Phone Number");
+            Console.WriteLine("Enter Password");
 
             string inStr = Console.ReadLine();
             string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]"; 
-            string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";//creating pattern for phone number
-            
-            Regex regex = new Regex(phoneNum);
+            string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
+            string password = @"[a-zA-Z]{8}$";//creating pattern for password
+
+            Regex regex = new Regex(password);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
