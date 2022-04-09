1405253ca92c927f81e82b25d3723881eab9f0a6
PSUMIT1995 <psumit191.sp@gmail.com>
2022-04-10 01:58:26 +0530

    UC4 : Enter Phone Number

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,14 +8,15 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Regex User Registration Problem");
-            Console.WriteLine("Enter EmailId");
+            Console.WriteLine("Enter Phone Number");
 
             string inStr = Console.ReadLine();
             string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]"; //creating pattern for emailid
+            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]"; 
+            string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";//creating pattern for phone number
             
-            Regex regex = new Regex(emailId);
+            Regex regex = new Regex(phoneNum);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
