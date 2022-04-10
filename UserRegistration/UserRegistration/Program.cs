59d0707e9247ea78d5f1cebd13b4559db0b74090
PSUMIT1995 <100415166+PSUMIT1995@users.noreply.github.com>
2022-04-10 13:00:16 +0530

    Update Program.cs

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -14,7 +14,7 @@ namespace UserRegistration
             string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
             string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]"; 
-            string phoneNum = @"[0-9]{2}\s+[1-9]{10}$";
+            string phoneNum = @"^([0-9]{2})\s([6-9]{1}[0-9]{9})$";
             string password = @"[a-zA-Z]{8}$";//creating pattern for password
 
             Regex regex = new Regex(password);
